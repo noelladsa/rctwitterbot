c0508a9ad0673352cb8329fe3aa5b69c97541d92
Erisa Dervishi <erisa85d@gmail.com>
2015-06-16 18:37:10 -0400

    json parser

--- twitter_interact.py
+++ twitter_interact.py
@@ -1,7 +1,20 @@
 import twython
 import sys
+import json
 
 api_key, api_secret, access_key, access_token = sys.argv[1:]
 
 twy = twython.Twython(api_key,api_secret,access_key, access_token)
-twitter.update_status(status="Blah")
+data = twy.get_user_timeline(screen_name="noelladsa", exclude_replies="true")
+
+#with open('test.json') as data_file:    
+ #   data = json.load(data_file)
+
+def getTwitterText (twitterResponseDict):
+    userTextList = []
+    for tweet in twitterResponseDict:
+         userTextList.append(tweet["text"])
+    return  userTextList
+
+print getTwitterText(data)  
+
